b82f83d85276f8eae8b4e3b1b0ced0adf542e632
Matija Osrecki <matija.osrecki@gmail.com>
2020-09-20 11:32:49 +0200

    added full custom event support

--- app.js
+++ app.js
@@ -3,20 +3,21 @@ const daysEl = document.querySelector('#days');
 const hoursEl = document.querySelector('#hours');
 const minutesEl = document.querySelector('#minutes');
 const secondsEl = document.querySelector('#seconds');
-const options = document.querySelectorAll('.options .option i');
+const options = document.querySelectorAll('.options .option');
 const modal = document.querySelector('.modal');
+const form = document.querySelector('.modal__form');
 const dateInput = modal.querySelector('#date-input');
 const nameInput = modal.querySelector('#name-input');
+const modalExit = modal.querySelector('.modal__exit');
 
-let ourEvent = { date: '2020-10-20' };
-// let ourEvent = { date: '2020-12-10', name: 'My Birthday' };
+// let ourEvent = { date: '2020-10-20' };
+let ourEvent = { date: '2020-12-10', name: 'My Birthday' };
 // let ourEvent = { date: '2022-12-25', name: 'Christmas 2022' }
 // let ourEvent = { year: 2022, name: 'Christmas' }
 // let ourEvent = { name: 'New Years Eve' }
 // let ourEvent = { name: 'UEFA Euro 2021' }
 
-startCountdown(ourEvent);
-window.intervalId = setInterval(() => startCountdown(ourEvent), 1000);
+startInterval(ourEvent);
 
 options.forEach(option => {
     option.addEventListener('click', () => {
@@ -32,20 +33,32 @@ options.forEach(option => {
                 countdownEvent = { name: 'UEFA Euro 2021' }
                 break;
             case 'custom':
-                modal.classList.toggle('show');
-                countdownEvent = getCustomEvent();
+                modal.classList.add('show');
         }
-        clearInterval(window.intervalId)
-        startCountdown(countdownEvent);
-        window.intervalId = setInterval(() => startCountdown(countdownEvent), 1000);
+
+        if (countdownEvent) {
+            startInterval(countdownEvent)
+        }
     })
 })
 
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    countdownEvent = getCustomEvent();
+    startInterval(countdownEvent);
+    modal.classList.remove('show');
+})
+
+modalExit.addEventListener('click', () => {
+    modal.classList.remove('show');
+})
+
 function getCustomEvent() {
-    let countdownEvent = {};
-    dateInput.value = countdownEvent.date;
-    nameInput.value = countdownEvent.name;
-    return countdownEvent;
+    const date = dateInput.value;
+    const name = nameInput.value;
+    console.log(dateInput.value);
+    return ({ date, name })
+
 }
 
 function startCountdown(event) {
@@ -55,6 +68,12 @@ function startCountdown(event) {
     let difference = date - currentDate;
     let countdownDate = calculateCountdown(difference)
     showCountdown(countdownDate, name);
+}
+
+function startInterval(countdownEvent) {
+    clearInterval(window.intervalId)
+    startCountdown(countdownEvent);
+    window.intervalId = setInterval(() => startCountdown(countdownEvent), 1000);
 }
 
 function recognizeEvent({ date, name, year }) {
@@ -77,21 +96,12 @@ function recognizeEvent({ date, name, year }) {
 
 function calculateCountdown(difference) {
     let totalSeconds = difference / 1000;
-    // const days = Math.trunc(difference / 86400000);
-    // difference -= days * 86400000;
-    // const hours = Math.trunc(difference / 3600000);
-    // difference -= hours * 3600000;
-    // const minutes = Math.trunc(difference / 60000)
-    // difference -= minutes * 60000;
-    // const seconds = Math.trunc(difference / 1000);
-    // difference -= seconds * 1000;
-    // let countdown = { days, hours, minutes, seconds }
-    // return countdown;
 
     const days = Math.trunc(totalSeconds / 3600 / 24);
     const hours = Math.trunc(totalSeconds / 3600) % 24;
     const minutes = Math.trunc(totalSeconds / 60) % 60
     const seconds = Math.trunc(totalSeconds) % 60;
+
     let countdown = { days, hours, minutes, seconds }
     return countdown;
 }
@@ -108,7 +118,7 @@ function showCountdown({ days, hours, minutes, seconds }, name) {
         document.body.classList.add('palms');
     }
 
-    eventEl.innerText = name || 'random thingamajig';
+    eventEl.innerText = name;
     daysEl.innerText = formatTime(days);
     hoursEl.innerText = formatTime(hours);
     minutesEl.innerText = formatTime(minutes);
@@ -116,6 +126,7 @@ function showCountdown({ days, hours, minutes, seconds }, name) {
 }
 
 function formatTime(time) {
+    if (time <= 0) return time;
     return time < 10 ? `0${time}` : time;
 }
 
